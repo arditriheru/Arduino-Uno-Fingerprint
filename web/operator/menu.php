c37aeee86a060d2f4a879a8bf2ed1614b9f0ab77
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-05-06 15:21:25 +0700

    Add files via upload

---
+++ web/operator/menu.php
@@ -0,0 +1,26 @@
+<!-- Sidebar -->
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand">Sistem Keamanan | <?php include '../date.php'; ?></a></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav side-nav">
+            <li class="active"><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li class="active"><a href="filter-aktifitas.php"><i class="fa fa-check-square-o"></i> Aktifitas</a></li>
+            <li class="active"><a href="user-tampil.php"><i class="fa fa-check-square-o"></i> User</a></li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right navbar-user">
+            
+                <li><a href="../logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+              
+          </ul>
