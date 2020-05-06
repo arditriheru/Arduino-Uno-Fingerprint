83a36dbcdfd94d397f44187745dbddf99439bc71
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-05-06 15:23:12 +0700

    Create fingerprint_delete.ino

---
+++ arduino/fingerprint_delete.ino
@@ -0,0 +1,71 @@
+#include <Adafruit_Fingerprint.h>
+
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3);
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void setup()  
+{
+  Serial.begin(9600);
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  Serial.println("\n\nDelete Finger");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Fingerprint Ditemukan!");
+  } else {
+    Serial.println("Fingerprint Tidak Ditemukan :(");
+    while (1);
+  }
+}
+
+
+uint8_t readnumber(void) {
+  uint8_t num = 0;
+  
+  while (num == 0) {
+    while (! Serial.available());
+    num = Serial.parseInt();
+  }
+  return num;
+}
+
+void loop()                     // run over and over again
+{
+  Serial.println("Masukkan ID # (from 1 to 127) yang akan dihapus...");
+  uint8_t id = readnumber();
+  if (id == 0) {// ID #0 not allowed, try again!
+     return;
+  }
+
+  Serial.print("Deleting ID #");
+  Serial.println(id);
+  
+  deleteFingerprint(id);
+}
+
+uint8_t deleteFingerprint(uint8_t id) {
+  uint8_t p = -1;
+  
+  p = finger.deleteModel(id);
+
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Deleted!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not delete in that location");
+    return p;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return p;
+  } else {
+    Serial.print("Unknown error: 0x"); Serial.println(p, HEX);
+    return p;
+  }   
+}
